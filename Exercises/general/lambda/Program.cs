0e905d49f37974e21ce2f5802309059ea6ccca49
Darius Vallejo <darius.vallejo@gmail.com>
2020-05-06 18:31:15 -0600

    Organizes stuff

---
+++ Exercises/general/lambda/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lambda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string p1Name;
+            string p2Name;
+            Console.Write("Please input player 1 name: ");
+            p1Name = Console.ReadLine();
+            Console.Write("Please input player 2 name: ");
+            p2Name = Console.ReadLine();
+
+            Console.WriteLine(p1Name);
+        }
+    }
+}
